416f5c14c4db531f57cd3aed6beb976f28186a63
HoThanhPhuc-CDTH19PMC <91512578+thanhphuc261@users.noreply.github.com>
2021-12-13 16:47:15 +0700

    done login ui

---
+++ flutter_travel_app/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'login.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: LogPage(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
